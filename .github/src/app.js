50dcc5d33f6df577e2384bbb228a8b8c9140a211
Nada Ahmed <nadaahmedsaad839@gmail.com>
2025-07-09 15:03:10 +0300

    Add initial simple app and test

---
+++ .github/src/app.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+module.exports = greet; // Make it reusable
+
+// Run directly
+if (require.main === module) {
+  // Read from JSON
+  const data = JSON.parse(fs.readFileSync('data.json', 'utf8'));
+  console.log(greet(data.name));
+}
